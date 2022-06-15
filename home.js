848ba3b34d73f06dacb0927c232c3fe2bfc8ec56
ppp821203 <ppp821203@gmail.com>
2022-06-15 17:18:27 +0900

    2

--- home.js
+++ home.js
@@ -1,3 +1,16 @@
-console.log('work')
+$(document).ready(function(){
+    var currentdate = new Date(); 
+    var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                    + (currentdate.getMonth()+1)  + "/" 
+                    + currentdate.getFullYear() + " @ "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+    
+    console.log('work2');
+    console.log(datetime);
+    
+    $(".asideRight").html('testtest00');
 
-$(".asideRight").html('testtest00');
+
+});
