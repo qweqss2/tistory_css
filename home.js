2c79a74abfc6b4dfdf96cca42a4fa71021a1a233
ppp821203 <ppp821203@gmail.com>
2022-06-15 17:42:00 +0900

    11

--- home.js
+++ home.js
@@ -7,8 +7,26 @@ $(document).ready(function(){
                     + currentdate.getMinutes() + ":" 
                     + currentdate.getSeconds();
     
-    console.log('work2');
     console.log(datetime);
     
-    $("#asideRight").html('data test' + datetime);
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.signal.bz/news/realtime',
+      dataType: 'json',
+      success: function(data) {
+    
+        var names = data.top10;
+        console.log(names);
+        p_text = ''
+        $.each(names, function(index, e) {
+          rk = e.rank;
+          kw = e.keyword;
+    
+          p_text += '<li>' + rk + ' : <a href=\"https://search.naver.com/search.naver?query='+kw+'\" target=\"_new\">' + kw + '</a></li>';
+        });
+    
+        $('#asideRight').html(p_text);
+      }
+    });
+    
 });
